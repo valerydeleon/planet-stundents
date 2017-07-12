4bd7fb16df0272f43293c7e42f6eeb6d77fcc79b
Valeria de Leon <valery.deleon@gmail.com>
2017-07-12 17:19:38 -0500

    materialize and card

--- server.js
+++ server.js
@@ -2,6 +2,7 @@ var express = require("express");
 var app = express();
 
 app.use("/data", express.static(__dirname + '/data'));
+app.use("/data", express.static(__dirname + '/node_modules'));
 
 app.get("/",function(req,res){
   res.sendFile(__dirname+"/index.html")
